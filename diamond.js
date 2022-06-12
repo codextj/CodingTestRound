67572fb8ca76446bc37d6fba835c9df3052cb69d
Tanmay Jain <tanmayjain.cs@gmail.com>
2022-06-12 20:01:25 +0530

    refactored code

--- diamond.js
+++ diamond.js
@@ -10,24 +10,32 @@ char = validateInput(char);
 charAsciiVal = char.charCodeAt();
 const N = charAsciiVal % 65 + 1;
 
-const getDiamondArr = (N) => {
-    const diamondArrLength = N + (N-1);
+const diamondArr = getContainerForDiamond(N);
+fillDiamondArray(diamondArr, N);
+printToConsole(diamondArr);
+
+function getContainerForDiamond(N) {
+    /*
+    N    : rows of upper half
+    N-1  : rows of lower half
+    Total rows of container => M : N + (N-1)
+    Container dimension : M x M 
+    */
+    const M = N + (N-1);
     let diamondArr = [];
-    for (let i=0; i<diamondArrLength; i++) {
+
+    for (let i=0; i<M; i++) {
         diamondArr.push([]);
-        for (let j=0; j<diamondArrLength; j++) {
+        for (let j=0; j<M; j++) {
             diamondArr[i].push(' ');
         }
     }
     return diamondArr;
 }
 
-const diamondArr = getDiamondArr(N);
-
-function fillDiamondArray(diamondArr, half, startRow, endRow) {
+function fillDiamondArray(diamondArr, N) {
     let mid = Math.floor((diamondArr.length-1) / 2);
     let asciiVal = 65;
-    let mode = half.toUpperCase();
 
     const fillRow = (i) => {
         // from start
@@ -39,19 +47,18 @@ function fillDiamondArray(diamondArr, half, startRow, endRow) {
         mid -= 1;
         asciiVal += 1;
     }
+    
+    // Fill upper half
+    for(let i=0; i<N; i++) {
+        fillRow(i);
+    }
 
-    if (mode == "UPPER") {
-        for(let i=startRow; i<endRow; i++) {
-            fillRow(i);
-        }
-    } else if (mode == "LOWER") {
-        for(let i=startRow; i>=endRow; i--) {
-            fillRow(i);
-        }
+    // Reset
+    mid = Math.floor((diamondArr.length-1) / 2);
+    asciiVal = 65;
+
+    // Fill lower half
+    for(let i=diamondArr.length-1; i>=N; i--) {
+        fillRow(i);
     }
 }
-
-fillDiamondArray(diamondArr, "upper", 0, N);
-fillDiamondArray(diamondArr, "lower", diamondArr.length-1, N);
-
-printToConsole(diamondArr);
