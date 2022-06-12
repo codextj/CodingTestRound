3890a2a6cb6e310a9575a74dc43a42968e5049f9
Tanmay Jain <tanmayjain.cs@gmail.com>
2022-06-12 19:00:50 +0530

    1. added logic for lower half fill 2. print diamond code

--- diamond.js
+++ diamond.js
@@ -28,7 +28,9 @@ console.log(diamondArr);
 let asciiVal = 65;
 
 // Fill upper half
-mid = Math.floor(N / 2);
+mid = Math.floor((diamondArr.length-1) / 2);
+console.log(N, mid)
+
 for(let i=0; i<N; i++) {
     console.log("mid >", mid);
     console.log("asciiVal >", asciiVal);
@@ -44,4 +46,29 @@ for(let i=0; i<N; i++) {
     console.log(">",diamondArr[i])
 }
 
-console.log("--",diamondArr)
+// Reset
+mid = Math.ceil((diamondArr.length-1) / 2);
+asciiVal = 65;
+
+console.log("/////////////////////////////////")
+
+
+// Fill lower half
+for(let i=diamondArr.length-1; i>=N; i--) {
+    console.log("mid >", mid);
+    console.log("asciiVal >", asciiVal);
+    
+    // from start
+    diamondArr[i][mid] = String.fromCharCode(asciiVal);
+
+    // from end
+    diamondArr[i][diamondArr.length-1 - mid] = String.fromCharCode(asciiVal);
+    
+    mid -= 1;
+    asciiVal += 1;
+    console.log(">",diamondArr[i])
+}
+
+for(let i=0; i<diamondArr.length; i++) {
+    console.log(diamondArr[i].join(""));
+}
