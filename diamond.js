c4472b7deebbd4deceaed7ae827d13629a875458
Tanmay Jain <tanmayjain.cs@gmail.com>
2022-06-12 18:46:09 +0530

    1.bug fix in diamond array creation 2. Logic to fill upper half

--- diamond.js
+++ diamond.js
@@ -12,9 +12,36 @@ const N = charAsciiVal % 65 + 1;
 
 const getDiamondArr = (N) => {
     const diamondArrLength = N + (N-1);
-    const tempArr =  new Array(diamondArrLength);
-    return tempArr.fill(new Array(diamondArrLength))
+    let diamondArr = [];
+    for (let i=0; i<diamondArrLength; i++) {
+        diamondArr.push([]);
+        for (let j=0; j<diamondArrLength; j++) {
+            diamondArr[i].push(' ');
+        }
+    }
+    return diamondArr;
 }
 
 const diamondArr = getDiamondArr(N);
 console.log(diamondArr);
+
+let asciiVal = 65;
+
+// Fill upper half
+mid = Math.floor(N / 2);
+for(let i=0; i<N; i++) {
+    console.log("mid >", mid);
+    console.log("asciiVal >", asciiVal);
+    
+    // from start
+    diamondArr[i][mid] = String.fromCharCode(asciiVal);
+
+    // from end
+    diamondArr[i][diamondArr.length-1 - mid] = String.fromCharCode(asciiVal);
+    
+    mid -= 1;
+    asciiVal += 1;
+    console.log(">",diamondArr[i])
+}
+
+console.log("--",diamondArr)
